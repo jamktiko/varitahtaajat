a19c0e3c485ed60c757705022c60a34cc8ef5e2a
Maria Karhu <AH4418@student.jamk.fi>
2026-04-28 13:38:58 +0300

    fix: move modal route files to correct directory

---
+++ src/routes/modal/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Modal from '..//Modal.svelte';
+
+  let open = $state(false);
+</script>
+
+<button onclick={() => (open = true)}>
+  Avaa modal
+</button>
+
+<Modal {open} onClose={() => (open = false)}>
+    <h2>Modal</h2>
+    <p>Malli pelin modaleihin</p>
+</Modal>
+    
+<style> 
+
+button {
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  cursor: pointer;
+}
+</style>
