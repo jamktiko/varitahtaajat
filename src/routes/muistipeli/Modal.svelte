d460c932d396100f0de303d429b6566cab6e6322
rayimove-e <rayimove444@gmail.com>
2026-04-30 12:19:55 +0300

    Modal osittain valmis

--- src/routes/muistipeli/Modal.svelte
+++ src/routes/muistipeli/Modal.svelte
@@ -11,10 +11,23 @@
 
 {#if open}
 	<button class="backdrop" aria-label="Close modal" onclick={onClose}> </button>
-
+	<link href="https://fonts.googleapis.com/css2?family=Jersey+10&display=swap" rel="stylesheet" />
 	<div class="modal">
-		{@render children?.()}
-		<button onclick={() => Close()}>Close</button>
+		<div class="content">
+			<div class="pointympyra">
+				<div class="pointit jersey-10-regular">6/10</div>
+			</div>
+			<div class="motivaatioteksti jersey-10-regular">Not bad. Try it again!</div>
+			<button class="nappulanextlevel">
+				<div class="nappulanextleveltx jersey-10-regular">Next Level</div>
+			</button>
+			<button class="nappularestart">
+				<div class="nappularestarttx jersey-10-regular">Restart</div>
+			</button>
+			<button class="nappulamenu">
+				<div class="nappulamenutx jersey-10-regular">Menu</div>
+			</button>
+		</div>
 	</div>
 {/if}
 
@@ -32,14 +45,60 @@
 	.modal {
 		padding: 1rem;
 		position: fixed;
-		top: 10vh;
-		left: 10vw;
-		width: 80%;
-		max-height: 80vh;
-		background: rgb(255, 255, 255);
-		border-radius: 5px;
+		width: 500px;
+		height: 500px;
+		aspect-ratio: 1/1;
+		border-radius: 10px;
+		background: #eeeeff;
 		z-index: 100;
 		box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+	}
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	.pointympyra {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100px;
+		height: 100px;
+		background: #1e1e1e;
+		border-radius: 50%;
+		margin-bottom: 1rem;
+	}
+	.pointit {
+		font-size: clamp(3rem, 2vw, 4rem);
+	}
+	.motivaatioteksti {
+		font-size: clamp(3rem, 2vw, 4rem);
+		margin-bottom: 2rem;
+		color: #1e1e1e;
+	}
+	.nappulanextlevel,
+	.nappularestart,
+	.nappulamenu {
+		padding: 0.5rem 1rem;
+		margin: 0.5rem;
+		width: clamp(200px, 15vw, 250px);
+		height: clamp(40px, 10vw, 50px);
+		background: #1e1e1e;
+		border: none;
+		border-radius: 5px;
+		cursor: pointer;
+	}
+	.nappulanextleveltx,
+	.nappularestarttx,
+	.nappulamenutx {
+		color: #ffffff;
+		font-size: clamp(2rem, 3vw, 3rem);
+	}
+	.jersey-10-regular {
+		font-family: 'Jersey 10', sans-serif;
+		font-weight: 500;
+		font-style: normal;
 	}
 
 	@media (min-width: 768px) {
