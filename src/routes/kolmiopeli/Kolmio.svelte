90aabbd7693dc4be54137a5536f42cffce7289e1
rayimove-e <rayimove444@gmail.com>
2026-05-06 17:43:47 +0300

    KolmioPeli (alku)

---
+++ src/routes/kolmiopeli/Kolmio.svelte
@@ -0,0 +1,41 @@
+<script>
+	import '$lib/kolmiopelistyles/kolmio.css';
+</script>
+
+<link href="https://fonts.googleapis.com/css2?family=Jersey+10&display=swap" rel="stylesheet" />
+<div class="pagekolmio">
+	<div class="containerkolmio">
+		<div class="jersey-10-regular kolmioteksti1">Press Start</div>
+		<div class="jersey-10-regular kolmioteksti1">70%</div>
+		<div class="playground">
+			<svg
+				class="spinnerkolmio"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 202 178"
+				fill="none"
+			>
+				<path
+					d="M92.2873 172.5C96.1363 179.167 105.759 179.167 109.608 172.5L200.54 15C204.389 8.33334 199.578 0 191.88 0H10.0149C2.31691 0 -2.49435 8.33333 1.35465 15L92.2873 172.5Z"
+					fill="#D8C092"
+				/>
+			</svg>
+		</div>
+		<div class="rgb-grid">
+			<div class="rgb-card">
+				<div class="rgb-letter jersey-10-regular">R</div>
+				<div class="rgb-box"></div>
+			</div>
+
+			<div class="rgb-card">
+				<div class="rgb-letter jersey-10-regular">G</div>
+				<div class="rgb-box"></div>
+			</div>
+
+			<div class="rgb-card">
+				<div class="rgb-letter jersey-10-regular">B</div>
+				<div class="rgb-box"></div>
+			</div>
+		</div>
+		<button class="kolmiomainbutton jersey-10-regular"> Submit </button>
+	</div>
+</div>
