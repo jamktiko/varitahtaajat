ba260dfaee382770c550ea22c8313c79f3618b9a
Eemeli, Latva-aho <ah4721@student.jamk.fi>
2026-05-06 10:42:19 +0300

    Sävypeli tehty

---
+++ src/routes/savypeli/Savyvalinta.svelte
@@ -0,0 +1,46 @@
+<script>
+    import '$lib/savypelistyles/savyvalinta.css'
+</script>
+
+<link href="https://fonts.googleapis.com/css2?family=Jersey+10&display=swap" rel="stylesheet" />
+
+<div class="containersavy"> 
+    <div class="savyvalintateksti_container">
+        <h1 class = "savyvalintateksti"> Valitse oikea väri </h1>
+    </div>
+<div class="varipallo"> </div>
+
+
+    <div class="slidercontainer">
+
+        <div class="slider">
+        <div class="sliderheader"> 
+            <span> Hue </span>
+            <span> 100° </span>
+        </div>
+            <input type="range" class="Hue">
+        </div>
+        
+        <div class="slider">
+        <div class="sliderheader"> 
+            <span> Saturation </span>
+            <span> 100% </span>
+        </div>
+            <input type="range" class="saturation">
+        </div>
+
+        <div class="slider">
+        <div class="sliderheader"> 
+            <span> Brightness </span>
+            <span> 100% </span>        
+        </div>
+            <input type="range" class="brightness">
+        </div>
+        
+    </div>
+
+</div>
+
+
+
+
