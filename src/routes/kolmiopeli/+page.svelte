4976a5f91f61dea1e8f0e42d7a0b4263d1eb3481
rayimove-e <rayimove444@gmail.com>
2026-05-06 17:43:47 +0300

    KolmioPeli (alku)

---
+++ src/routes/kolmiopeli/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '$lib/kolmiopelistyles/kolmiopeliohje.css';
+	import '$lib/kolmiopelistyles/kolmio.css';
+	import Kolmiopeliohje from './Kolmiopeliohje.svelte';
+	import Kolmio from './Kolmio.svelte';
+</script>
+
+<!-- <Kolmiopeliohje></Kolmiopeliohje> -->
+<Kolmio></Kolmio>
