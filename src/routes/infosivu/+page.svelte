84ba061abbff75ddc3dba793d07ad3936d31b2b3
Eemeli, Latva-aho <ah4721@student.jamk.fi>
2026-05-08 13:26:36 +0300

    Korjattu juttuja ja parannuksia nappi toimintaan

--- src/routes/infosivu/+page.svelte
+++ src/routes/infosivu/+page.svelte
@@ -6,9 +6,17 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=anonymous>
 <link href="https://fonts.googleapis.com/css2?family=Jersey+10&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=arrow_back_ios" />
+
 
 
 <div class="container">
+
+<a href={resolve("/")} class="backbutton" data-sveltekit-reload>
+    <span class="material-symbols-outlined nuoliback">
+        arrow_back_ios
+    </span>
+</a>
 
 
     <header class="otsikko">
@@ -20,9 +28,8 @@
         <section class ="sisalto">
             <h2>Tietoa sovelluksesta</h2>
 
-            <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Aliquam qui voluptas inventore quibusdam hic aperiam, atque eveniet quidem dolorum exercitationem, 
-                quos amet nostrum magnam. Laudantium minus voluptatibus soluta numquam quae!
+            <p> Väritähtääjä peli on kouluprojektina tuotettu peli, tekijät ovat JAMK:in opiskelijoita.
+                 Pelissä on kolme eri tasoa, joissa on eri tavoite. Peli sopii kaiken ikäisille. Peli on tuotettu osana opintoja. 
             </p>
 
         </section>
@@ -51,7 +58,7 @@
                 <li> Visual Studio Code / Svelte </li> 
                 <li> TypeScript </li> 
                 <li> Figma </li> 
-                <li> Netlify </li>
+                <li> Vercel </li>
                 <li> Trello </li> 
                 <li> Microsoft Teams </li> 
                 <li> Microsoft PowerPoint </li> 
